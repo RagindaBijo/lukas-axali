f4487fb8f50fa0bf280904f24a351533e3f4a235
Luka <115728454+RagindaBijo@users.noreply.github.com>
2022-10-21 00:41:07 +0400

    Add files via upload

---
+++ main/src/main/kotlin/sadadsad.kt
@@ -0,0 +1,40 @@
+fun main(){
+    generic(2.2,3.5,4.6,)
+}
+
+interface Number{
+    fun moqmedebebi_ricxvebze() : Double
+}
+
+
+
+class generic<in T,A,B>(num1: T,num2:A,num3:B) : Number {
+
+
+
+}
+
+
+
+enum class operacia(var operatori:String){
+    gamravleba("*"),
+    gayopa("/"),
+    moduli("%")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
